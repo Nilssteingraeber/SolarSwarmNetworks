40a901982ea4dc1cda870f14ae9b4b0547144a8e
VincentWelsch <vincent.welsch@stud.hs-bochum.de>
2025-09-28 10:15:57 +0000

    Delete ros2_workspaces/sw_robot_init directory

--- ros2_workspaces/sw_robot_init/sw_robot.Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM ros:jazzy
-COPY sw_robot_init /home/ubuntu/solarswarm/
-COPY sw_robot_install.zip /home/ubuntu/solarswarm/
-COPY unzip.py /home/ubuntu/solarswarm/
-WORKDIR /home/ubuntu/solarswarm
-SHELL ["/bin/bash", "-c"]
-ENTRYPOINT ["bash", "mock_data_init.bash"]
--- ros2_workspaces/sw_robot_init/sw_robot_init.bash
+++
@@ -1,7 +0,0 @@
-source /opt/ros/jazzy/setup.bash
-# rosdep install -i --from-path src --rosdistro jazzy -y
-# colcon build --packages-select custom_interfaces
-# colcon build --packages-select mock_robot
-python3 unzip.py "sw_robot_install.zip"
-source sw_robot_init/local_setup.bash
-ros2 run sw_robot data_sink
