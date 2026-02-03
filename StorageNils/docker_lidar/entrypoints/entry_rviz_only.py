31162ce5610f039cd66c570e44db95fc4ba61025
Nils <nils.steingraeber@stud.hs-bochum.de>
2026-02-03 15:36:16 +0100

    Cleanup: StorageNils entfernt

--- StorageNils/docker_lidar/entrypoints/entry_rviz_only.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import time
-
-setup = 'source /opt/ros/jazzy/setup.bash && source /lidar_node_ws/install/setup.bash &&'
-rviz_cmd = f'{setup} ros2 run rviz2 rviz2'
-rviz_proc = subprocess.Popen(rviz_cmd, shell=True, executable='/bin/bash')
-rviz_proc.wait()
